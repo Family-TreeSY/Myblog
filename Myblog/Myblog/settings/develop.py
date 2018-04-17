1067170e342d70add1c644829a9032c02fe85fc3
Family-TreeSY <286210002@qq.com>
2018-04-17 19:15:36 +0800

    设置开发环境

---
+++ Myblog/Myblog/settings/develop.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+from .base import * # noqa
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- Myblog/manage.py
+++ Myblog/manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Myblog.settings")
+    profile = os.environ.get('MYBLOG_PROFILE', 'develop')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Myblog.settings.%s" % profile)
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
