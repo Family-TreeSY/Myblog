82dfad35d71139cc240e5891736ae306daed81f0
Family-TreeSY <286210002@qq.com>
2018-04-17 19:04:01 +0800

    完成config和comment应用admin开发

--- Myblog/comment/admin.py
+++ Myblog/comment/admin.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "target",
+        "nickname",
+        "status",
+        "email",
+        "created_time",
+    ]
+    fields = (
+        ("target", "status"),
+        "content",
+        "website",
+        "email",
+    )
--- Myblog/config/admin.py
+++ Myblog/config/admin.py
@@ -3,4 +3,37 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Link, SideBar
+
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "href",
+        "weight",
+        "author",
+        "created_time",
+    ]
+    fields = (
+        ("title", "weight"),
+        "href",
+        "author",
+    )
+
+
+@admin.register(SideBar)
+class SideBarAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "status",
+        "display_type",
+        "author",
+        "created_time",
+    ]
+    fields = (
+        ("title", "status"),
+        "display_type",
+        "author",
+        "content",
+    )
