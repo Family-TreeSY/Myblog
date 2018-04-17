7c5145f7a82c286343a3015f742f91c60c254a9c
Family-TreeSY <286210002@qq.com>
2018-04-17 12:22:13 +0800

    完成config模型,侧边栏和友链

--- Myblog/config/models.py
+++ Myblog/config/models.py
@@ -2,5 +2,59 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Link(models.Model):
+    """友链"""
+    STATUS_ITEMS = (
+        (1, "正常"),
+        (2, "删除"),
+    )
+    title = models.CharField(max_length=50, verbose_name="名称")
+    href = models.URLField(verbose_name="链接")
+    status = models.PositiveIntegerField(
+        default=1, choices=STATUS_ITEMS, verbose_name="状态"
+    )
+    weight = models.PositiveIntegerField(
+        default=1, choices=zip(
+            range(1, 6), range(1, 6),
+        ), verbose_name="权重", help_text="权重越高展示顺序越靠前"
+    )
+    author = models.ForeignKey(User, verbose_name="作者")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "友链"
+
+
+class SideBar(models.Model):
+    """侧边栏"""
+    STATUS_ITEMS = (
+        (1, "展示"),
+        (2, "下线")
+    )
+    SIDE_TYPE = (
+        (1, "HTML"),
+        (2, "最新文章"),
+        (3, "最热文章"),
+        (4, "最热评论"),
+    )
+    title = models.CharField(max_length=50, verbose_name="标题")
+    status = models.PositiveIntegerField(
+        default=1, choices=STATUS_ITEMS, verbose_name="状态"
+    )
+    display_type = models.PositiveIntegerField(
+        default=1, choices=SIDE_TYPE, verbose_name="展示类型"
+    )
+    content = models.CharField(
+        max_length=100,
+        blank=True,
+        verbose_name="内容",
+        help_text="如果设置的不是HTML类型，可为空"
+    )
+    author = models.ForeignKey(User, verbose_name="作者")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "侧边栏"
