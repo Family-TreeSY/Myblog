110efd34583aeca9a99854adbaf4bb45d1ac129b
Family-TreeSY <286210002@qq.com>
2018-04-17 12:24:39 +0800

    完成comment模型

--- Myblog/comment/models.py
+++ Myblog/comment/models.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    STATUS_ITEMS = (
+        (1, '正常'),
+        (2, '删除'),
+    )
+    target = models.CharField(max_length=200, null=True, verbose_name='评论目标')
+    content = models.CharField(max_length=2000, verbose_name='内容')
+    nickname = models.CharField(max_length=50, verbose_name='昵称')
+    status = models.PositiveIntegerField(
+        default=1, choices=STATUS_ITEMS, verbose_name='状态'
+    )
+    website = models.URLField(verbose_name='网站')
+    email = models.EmailField(verbose_name='邮箱')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    class Meta:
+        verbose_name = verbose_name_plural = '评论'
