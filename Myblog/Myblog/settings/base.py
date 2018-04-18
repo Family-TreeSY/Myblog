a52c13580c5ac2216742cb6d566962f6e88fe536
Family-TreeSY <286210002@qq.com>
2018-04-18 14:53:12 +0800

    初探视图，创建url、view

--- Myblog/Myblog/settings/base.py
+++ Myblog/Myblog/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'blog',
     'config',
     'comment',
+    'Myblog',
 
     'django.contrib.admin',
     'django.contrib.auth',
--- Myblog/blog/views.py
+++ Myblog/blog/views.py
@@ -3,4 +3,23 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request, category_id=None, tag_id=None):
+    """render()方法根据我们传入的参数来构造Httpresponse
+        1、render传入Http请求后，根据第二个参数blog/list.html找到这个模板并且读取它的值
+        2、context是字典数据，传递到模板
+    """
+    context = {
+        "post_list": post_list,
+    }
+    return render(request, "blog/list.html", context=context)
+
+
+def post_detail(request, pk=None):
+    """pk就是post_id
+    name的值会被传到模板中
+    """
+    context = {
+        "name": "Treehl",
+    }
+    return render(request, "blog/detail.html", context=context)
