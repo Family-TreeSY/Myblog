73b580c9774ecef5363f5d29119fe39c2ab6ace3
Family-TreeSY <286210002@qq.com>
2018-04-19 13:47:02 +0800

    增加ListView, DetailView,,完成cbv基本设置

--- Myblog/blog/views.py
+++ Myblog/blog/views.py
@@ -1,13 +1,50 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import Http404
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView, DetailView
 
 from .models import Post, Tag, Category
 from config.models import SideBar
 from comment.models import Comment
+
+
+class BasePostView(ListView):
+    """
+    IndexView,CategoryView,TagView都是指向文章post的，所以这里写一个父类BasePostView
+    """
+    model = Post
+    template_name = "blog/list.html"
+    context_object_name = "posts"
+
+
+class IndexView(BasePostView):
+    pass
+
+
+class CategoryView(BasePostView):
+    def get_queryset(self):
+        qs = super(CategoryView, self).get_queryset()
+        # self.kwargs:获取url中的名字,(?P<category_id>\d+)
+        cate_id = self.kwargs.get("category_id")
+        qs = qs.filter(category_id=cate_id)
+        return qs
+
+
+class TagView(BasePostView):
+    def get_queryset(self):
+        tag_id = self.kwargs.get("tag_id")
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            return []
+        posts = tag.posts.all()
+        return posts
+
+
+class PostView(DetailView):
+    model = Post
+    template_name = "blog/detail.html"
+    context_object_name = "post"
 
 
 def get_common_context():
@@ -39,60 +76,3 @@ def get_common_context():
         "recently_comments": recently_comments,
     }
     return context
-
-
-def post_list(request, category_id=None, tag_id=None):
-    """render()方法根据我们传入的参数来构造Httpresponse
-        1、render传入Http请求后，根据第二个参数blog/list.html找到这个模板并且读取它的值
-        2、context是字典数据，传递到模板
-    """
-    # 从第一页开始
-    page = request.GET.get("page", 1)
-    # 每页数据量
-    page_size = 4
-    try:
-        page = int(page)
-    except TypeError:
-        page = 1
-
-    queryset = Post.objects.all()
-    if category_id:
-        queryset = queryset.filter(category_id=category_id)
-    elif tag_id:
-        try:
-            tag = Tag.objects.get(id=tag_id)
-        except Tag.DoesNotExist:
-            queryset = []
-        else:
-            queryset = tag.posts.all()
-
-    # 创建Paginator实例,每页展示4篇文章
-    paginator = Paginator(queryset, page_size)
-    try:
-        posts = paginator.page(page)
-    except EmptyPage:
-        # 页面超过页数范围就传递最后一页数据
-        posts = paginator.page(paginator.num_pages)
-
-    context = {
-        "posts": posts,
-    }
-    common_context = get_common_context()
-    context.update(common_context)
-    return render(request, "blog/list.html", context=context)
-
-
-def post_detail(request, pk=None):
-    """pk就是post_id
-    name的值会被传到模板中
-    """
-    try:
-        queryset = Post.objects.get(pk=pk)
-    except queryset.DoesNotExist:
-        return Http404("Post is not exist!")
-    context = {
-        "post": queryset,
-    }
-    common_context = get_common_context()
-    context.update(common_context)
-    return render(request, "blog/detail.html", context=context)
