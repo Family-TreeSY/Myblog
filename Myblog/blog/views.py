98e123f18e62bd7db0eff7050e4b7a7084edbca9
Family-TreeSY <286210002@qq.com>
2018-04-19 16:40:34 +0800

    完成CBV通用代码

--- Myblog/blog/views.py
+++ Myblog/blog/views.py
@@ -8,7 +8,43 @@ from config.models import SideBar
 from comment.models import Comment
 
 
-class BasePostView(ListView):
+class CommonMixin(object):
+    def get_category_context(self):
+        """分类
+            1、nav_cates = categoryies.filter(is_nav=True) 导航分类
+            2、cates = categories.filter(is_nav=False) 普通分类
+            3、Category.objects.all()返回一个queryset对象复制给categories
+        """
+        categories = Category.objects.all()
+        cates = []
+        nav_cates = []
+        for category in categories:
+            if category.is_nav:
+                nav_cates.append(category)
+            else:
+                cates.append(category)
+        return {
+            "nav_cates": nav_cates,
+            "cates": cates,
+        }
+
+    def get_context_data(self, **kwargs):
+        """
+        侧边栏
+        """
+        sidebars = SideBar.objects.filter(status=1)
+        recently_posts = Post.objects.filter(status=1)[:5]
+        recently_comments = Comment.objects.filter(status=1)[:2]
+        kwargs.update({
+            "sidebars": sidebars,
+            "recently_posts": recently_posts,
+            "recently_comments": recently_comments,
+        })
+        kwargs.update(self.get_category_context())
+        return super(CommonMixin, self).get_context_data(**kwargs)
+
+
+class BasePostView(CommonMixin, ListView):
     """
     IndexView,CategoryView,TagView都是指向文章post的，所以这里写一个父类BasePostView
     """
@@ -23,6 +59,12 @@ class IndexView(BasePostView):
 
 
 class CategoryView(BasePostView):
+    """覆写get_queryset方法
+    1、该方法默认获取指定模型的全部列表数据,为了获取指定分类下的文章列表数据，我们覆写该方法，改变它的默认行为
+    2、self.kwargs从url中得到category_id
+    3、get_queryset获取全部文章后再使用filter来过滤分类并返回
+    """
+
     def get_queryset(self):
         qs = super(CategoryView, self).get_queryset()
         # self.kwargs:获取url中的名字,(?P<category_id>\d+)
@@ -42,38 +84,7 @@ class TagView(BasePostView):
         return posts
 
 
-class PostView(DetailView):
+class PostView(CommonMixin, DetailView):
     model = Post
     template_name = "blog/detail.html"
     context_object_name = "post"
-
-
-def get_common_context():
-    """分类
-        1、nav_cates = categoryies.filter(is_nav=True) 导航分类
-        2、cates = categories.filter(is_nav=False) 普通分类
-        3、Category.objects.all()返回一个queryset对象复制给categories
-        """
-    categories = Category.objects.all()
-    cates = []
-    nav_cates = []
-    for category in categories:
-        if category.is_nav:
-            nav_cates.append(category)
-        else:
-            cates.append(category)
-
-    """
-    侧边栏
-    """
-    sidebars = SideBar.objects.filter(status=1)
-    recently_posts = Post.objects.filter(status=1)[:5]
-    recently_comments = Comment.objects.filter(status=1)[:2]
-    context = {
-        "nav_cates": nav_cates,
-        "cates": cates,
-        "sidebars": sidebars,
-        "recently_posts": recently_posts,
-        "recently_comments": recently_comments,
-    }
-    return context
