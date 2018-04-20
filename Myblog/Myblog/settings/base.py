a7d91429fe4b195fe0530b3067d53e8123254469
Family-TreeSY <286210002@qq.com>
2018-04-20 21:32:47 +0800

    修改模板路径

--- Myblog/Myblog/settings/base.py
+++ Myblog/Myblog/settings/base.py
@@ -56,10 +56,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Myblog.urls'
 
+THEME = 'themes/default'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, THEME, 'template')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
