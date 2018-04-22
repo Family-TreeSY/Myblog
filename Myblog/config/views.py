eb50ac95c4aa0c758c4db37b39ed90214bc591d1
Family-TreeSY <286210002@qq.com>
2018-04-22 21:36:39 +0800

    在links友链页面加入留言板

--- Myblog/config/views.py
+++ Myblog/config/views.py
@@ -5,6 +5,8 @@ from django.views.generic import ListView
 
 from blog.views import CommonMixin
 from .models import Link
+from comment.forms import CommentForm
+from comment.models import Comment
 
 
 class LinkView(CommonMixin, ListView):
@@ -13,3 +15,15 @@ class LinkView(CommonMixin, ListView):
     model = Link
     template_name = "config/link.html"
     context_object_name = "links"
+
+    def get_comment(self):
+        target = self.request.path
+        comments = Comment.objects.filter(target=target)
+        return comments
+
+    def get_context_data(self, **kwargs):
+        kwargs.update({
+            'comment_form': CommentForm(),
+            'comment_list': self.get_comment,
+        })
+        return super(LinkView, self).get_context_data(**kwargs)
