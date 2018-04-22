7e490ba3f7cc7ddad65c399a44f7bfc11dce45f0
Family-TreeSY <286210002@qq.com>
2018-04-22 21:06:03 +0800

    修改评论框

--- Myblog/comment/forms.py
+++ Myblog/comment/forms.py
@@ -7,13 +7,38 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    nickname = forms.CharField(
+        label="昵称",
+        max_length=100,
+        widget=forms.widgets.Input(
+            attrs={
+                'class': 'form-control',
+                'style': 'width: 45%'}))
+
+    email = forms.CharField(
+        label="邮箱",
+        max_length=100,
+        widget=forms.widgets.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'style': 'width: 45%'}))
+
+    website = forms.CharField(
+        label="站点",
+        max_length=100,
+        widget=forms.widgets.URLInput(
+            attrs={
+                'class': 'form-control',
+                'style': 'width: 45%'}))
+
     content = forms.CharField(
         label="内容",
         max_length=500,
         widget=forms.widgets.Textarea(
             attrs={
                 "rows": 6,
-                "cols": 88}))
+                "cols": 88,
+                "class": "form-control"}))
 
     def clean_content(self):
         """留言内容必须大于5个字符"""
